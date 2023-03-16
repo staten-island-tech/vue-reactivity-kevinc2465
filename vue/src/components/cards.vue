a30d575f017cfaf910be3aae876a790ba78017b6
kevinc2465 <chenk5104@gmail.com>
2023-03-16 13:13:14 -0400

    copy some codes

---
+++ vue/src/components/cards.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- <div class="itemCard">
+    <h2>{{ title }}</h2>
+  </div> -->
+</template>
+
+<script>
+// export default {
+//   name: 'itemCard',
+//   props: {
+//     title: String
+//   }
+// }
+</script>
