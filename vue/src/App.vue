4582b4fb2be87387613c712660cab0d3a47711bf
kevinc2465 <chenk5104@gmail.com>
2023-03-31 12:53:03 -0400

    Revert "Pray #1"
    This reverts commit 0aa38e97c49940dfd01f10fdc1b9633b08251b5b.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,20 +3,10 @@
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">Cart</RouterLink>
   </nav>
-  <!-- <ol>
-    <TodoItem v-for="item in groceryList" :todo="item" :key="item.id"></TodoItem>
-  </ol> -->
 </template>
 
 <script setup>
-// import { ref } from 'vue'
-// import TodoItem from './components/TodoItem.vue'
-
-// const groceryList = ref([
-//   { id: 0, text: 'Vegetables' },
-//   { id: 1, text: 'Cheese' },
-//   { id: 2, text: 'Whatever else humans are supposed to eat' }
-// ])
+import { RouterLink } from 'vue-router'
 </script>
 
 <style scoped>
