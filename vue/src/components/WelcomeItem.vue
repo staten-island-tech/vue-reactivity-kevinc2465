32c03f63e1a41a9d482f522fd26aace1dee61e15
kevinc2465 <chenk5104@gmail.com>
2023-04-03 20:46:29 -0400

    added an extra image

--- vue/src/components/WelcomeItem.vue
+++ vue/src/components/WelcomeItem.vue
@@ -3,6 +3,13 @@
     {{ text }}
     <a href="https://www.siths.org/apps/email/index.jsp?uREC_ID=1284494"> Michael Whalen</a>
   </p>
+  <h2>
+    Total Amount:
+    <img
+      src="https://media.istockphoto.com/id/1012682886/vector/infinity-symbol-black-simple-with-discontinuation-isolated-vector.jpg?s=612x612&w=0&k=20&c=CFVfjIg4yAHCPFpBq_wWjkZtc7GekfGO6nfqjI9CQM4="
+      alt="infinite picture"
+    />
+  </h2>
 </template>
 
 <script setup>
@@ -13,5 +20,14 @@ const text = 'If you really want to get smarter, contact and bribe the following
 .quote {
   font-size: 5rem;
   text-align: center;
+}
+h2 {
+  text-align: center;
+  font-size: 4rem;
+}
+img {
+  width: 20%;
+  text-align: center;
+  margin-top: 2%;
 }
 </style>
